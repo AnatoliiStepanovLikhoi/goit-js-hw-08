b43d38183a6dc97d63b6f8f8e5f681480828d70e
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-10-22 13:48:52 +0300

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,32 @@
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
+import imageTemplate from './gallery-template.hbs' 
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(...galleryItems);
+
+const galleryDivRef = document.querySelector('.gallery');
+
+galleryDivRef.insertAdjacentHTML('beforeend', imageTemplate({ galleryItems }))
+
+// const galleryMarkup = createGalleryItems(galleryItems);
+
+// galleryDivRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.9,
+    showCounter: false,
+    disableRightClick: true,
+    disableScroll: true,
+    // spinner: false,
+    // nav: false,
+ });
+
+// function createGalleryItems(items) {
+//     return items.map(item => imageTemplate(item)).join('');
+// };
