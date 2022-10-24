ffcf57917e277d49f8dad034c24f679f64c0714c
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-10-24 22:14:48 +0300

    removed global variable, new formData added

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,30 +10,23 @@ refs.formRef.addEventListener('input', throttle(onDataInput, 700));
 refs.formRef.addEventListener('submit', onFormSubmit);
 
 const FORM_DATA_KEY = "feedback-form-state";
-
 // let formData = JSON.parse(localStorage.getItem(FORM_DATA_KEY)) ?? {};
-
-let formData = {};
+// let formData = {};
 
 
 function returnFromLocalStorage() {
-
     let savedData = JSON.parse(localStorage.getItem(FORM_DATA_KEY));
 
     if (savedData) {
         console.log(savedData);
-
         refs.textareaRef.value = savedData.message ?? '';
         refs.emailInputRef.value = savedData.email ?? '';
     };
 
     // console.log(Object.values(formData));
-
     // if (Object.values(formData).length = 0)
     //  return;
-    
     // console.log(formData);
-
     // refs.textareaRef.value = formData.message;
     // refs.emailInputRef.value = formData.email;
 };
@@ -41,30 +34,29 @@ function returnFromLocalStorage() {
 returnFromLocalStorage();
 
 function onDataInput(event) {
+    let savedData = localStorage.getItem(FORM_DATA_KEY);
+    savedData = savedData ? JSON.parse(savedData) : {};
+    savedData[event.target.name] = event.target.value;
+    localStorage.setItem(FORM_DATA_KEY, JSON.stringify(savedData));
 
-    const inputValue = event.target.value;
-
-    formData[event.target.name] = inputValue;
-
-    // console.log(formData);
-
-    localStorage.setItem(FORM_DATA_KEY, JSON.stringify(formData));
-
+    // const inputValue = event.target.value;
+    // formData[event.target.name] = inputValue;
+    // // console.log(formData);
+    // localStorage.setItem(FORM_DATA_KEY, JSON.stringify(formData));
 };
 
 function onFormSubmit(event) {
     event.preventDefault();
+    // console.log(formData)
+    const formData = new FormData(refs.formRef);
 
-    console.log(formData)
-
+    formData.forEach((value, name) => console.log(name, value))
     onFormReset(event);
 };
 
 function onFormReset(event) {
     event.target.reset();
-
-    formData = {};
-    
+    // formData = {};
     localStorage.removeItem(FORM_DATA_KEY);
 };
 
