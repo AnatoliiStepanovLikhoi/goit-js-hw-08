354257a3ef6ebb56ef871894f9c2ffabed1cf95e
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-10-24 10:37:21 +0300

    videoplayer replaced options

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,17 @@
 import Player from '@vimeo/player';
 import throtle from 'lodash.throttle';
 
-const iframe = document.querySelector('#vimeo-player');
-const player = new Player(iframe, options);
-// const throttle = require('lodash.throttle');
-const durationTime = 'current-time-play'
-const savedTime = localStorage.getItem(durationTime);
-
 const options = {
     quality: "720p",
     autoplay: true,
     controls: false,
 };
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe, options);
+// const throttle = require('lodash.throttle');
+const durationTime = 'current-time-play'
+const savedTime = localStorage.getItem(durationTime);
 
 player.on('timeupdate', throtle(onPlay, 1000));
 
